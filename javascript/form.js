493be5961e75f516db62ec1b8043b5fef4437e79
mouringh <mouringh92@gmail.com>
2019-01-31 18:51:48 +0100

    Initial commit

---
+++ javascript/form.js
@@ -0,0 +1,23 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+
+
+
+
+//function validateForm() {
+//    var x = document.forms["myForm"]["voornaam"].value;
+  //  if (x == "") {
+  //      alert("Name must be filled out");
+  //      return false;
+ //   }
+//}
